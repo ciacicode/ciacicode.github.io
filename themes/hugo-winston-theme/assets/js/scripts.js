cf115df1ed68a6524934e5c779e3e6f12bcb6b7b
ciacicode <maria.cerase@gmail.com>
2023-06-25 23:14:31 +0100

    added the theme back

---
+++ themes/hugo-winston-theme/assets/js/scripts.js
@@ -0,0 +1,55 @@
+var body = document.querySelector('body');
+var menuTrigger = document.querySelector('#toggle-menu-main-mobile');
+var menuContainer = document.querySelector('#menu-main-mobile');
+var hamburgerIcon = document.querySelector('.hamburger');
+
+if (menuTrigger !== null) {
+  menuTrigger.addEventListener('click', function(e) {
+    menuContainer.classList.toggle('open');
+    hamburgerIcon.classList.toggle('is-active');
+    body.classList.toggle('lock-scroll');
+  });
+}
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["leader", "coach", "maker", "manager"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 3000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
