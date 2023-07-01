7f9415b8c77f4220f7d619fce0f215ad801af359
ciacicode <maria.cerase@gmail.com>
2023-07-01 15:10:16 +0100

    added some responsiveness fixes

--- themes/hugo-winston-theme/assets/js/scripts.js
+++ themes/hugo-winston-theme/assets/js/scripts.js
@@ -18,7 +18,7 @@ const cursorSpan = document.querySelector(".cursor");
 const textArray = ["leader", "coach", "maker", "manager"];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 3000; // Delay between current and next text
+const newTextDelay = 2000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
